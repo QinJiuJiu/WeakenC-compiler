5873c107ac351db644d98fb6c00a2b5480848fc6
kfmy-jy <56213118+kfmy-jy@users.noreply.github.com>
2020-05-29 00:00:41 +0800

    Add files via upload
    parser 第三部分
    block
    codeOptimize

---
+++ code/block.py
@@ -0,0 +1,34 @@
+class varNode:
+	  def __init__(self):
+		self.name = ""
+		self.type = ""
+		self.num=-1
+		self.useAddress=False
+		self.boolString=""
+        
+
+class funcNode:
+	def __init__(self):
+		self.isdefined=False
+		self.name=""
+		self.rtype=""
+		self.paralist=[]
+		
+
+class arrayNode:
+	def __init__(self ):
+		self.name = ""
+		self.type = ""
+		self.num=-1
+	
+
+class Block:
+	def __init__(self, name="", type="", num=-1 ):
+		self.func = funcNode()
+		self.isfunc=False 
+		self.varMap={}
+		self.arrayMap={}
+		self.breakLabelname=""
+		sekf.canBreak=False
+
+
