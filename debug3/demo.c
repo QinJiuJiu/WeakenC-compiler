48a1d4b9145fe4920104e1dce353cc100e39dd54
zyt <1501344179@qq.com>
2020-06-02 03:45:21 -0700

    final

--- debug3/demo.c
+++ debug3/demo.c
@@ -1,19 +1,33 @@
-int factorial(int i) {
-    if (i <= 1) {
-        return 1;
-    }
-    else{
-        return i * factorial(i - 1);
-    }
-}
-int main() {
-    int i, times, result;
-    times=read();
-    for(i = 1; i < times + 1; i++)
-    {
-        int temp;
-		temp = read();
-		result = factorial(temp);
-        print(result);
+int fib(int n)
+{
+	if (n == 1)
+		return 1;
+	else if (n == 2)
+		return 1;
+	else
+	{
+		return fib(n - 1) + fib(n - 2);
 	}
+}
+int main()
+{
+	int times, result;
+	times = read();
+	int i;
+	for(i=1;i<=times;i++)
+	{
+		int a;
+		a = read();
+		if(a >= 1)
+		{
+			result = fib(a);
+		}
+		else
+		{
+			break;
+		}
+		print(result);
+		
+	}
+	
 }
